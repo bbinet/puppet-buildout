e92dfccd217d0de7c33283102240fb3ff2965e84
Bruno Binet <binet.bruno@gmail.com>
2011-04-15 16:36:06 +0200

    class buildout that provide a 'venv' define to setup new buildouts environments

---
+++ manifests/init.pp
@@ -0,0 +1,61 @@
+# Class: buildout
+#
+# This module manages buildout and provides a define for setting up buildout
+# environments.
+#
+class buildout {
+
+    package { "python": }
+
+    # Definition: buildout::venv
+    #
+    # setup buildout virtual python environment
+    #
+    # Parameters:
+    #   $source  - source from which to grab buildout.cfg
+    #   $ensure  - flag to setup or remove the buildout environment
+    #
+    # Actions:
+    #   setup buildout virtual python environment
+    #
+    # Requires:
+    #   $source must be set
+    #
+    # Sample Usage:
+    #
+    #    buildout::venv { "/path/to/buildout_env":
+    #        source  => "puppet:///files/mybuildout.cfg",
+    #    }
+    #
+    define venv($source, $ensure=present) {
+        if $ensure == present {
+            exec { "mkdir -p $name":
+                unless => "test -d $name",
+            }
+            file { "$name/bootstrap.py":
+                source => "puppet:///modules/buildout/bootstrap.py",
+                require => Exec["mkdir -p $name"],
+            }
+            file { "$name/buildout.cfg":
+                source => $source,
+                require => Exec["mkdir -p $name"],
+            }
+            exec { "python bootstrap.py":
+                cwd => $name,
+                require => [File["$name/bootstrap.py"], File["$name/buildout.cfg"]],
+                unless => "test -f $name/bin/buildout",
+            }
+            exec { "$name/bin/buildout":
+                cwd => $name,
+                require => Exec["python bootstrap.py"],
+                subscribe => File["$name/buildout.cfg"],
+                refreshonly => true,
+            }
+        } else {
+            file { "$name":
+                ensure => absent,
+            }
+        }
+    }
+}
+
